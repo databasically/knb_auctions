41abbc8acdc05cc8c02d8d63a10714aafebf49a2
Chad Metcalf <chad@databasically.com>
2012-11-02 13:57:41 -0500

    Dashboard

---
+++ app/controllers/knb_auction/dashboard_controller.rb
@@ -0,0 +1,11 @@
+require_dependency "knb_auction/application_controller"
+
+module KnbAuction
+  class DashboardController < ApplicationController
+    def index
+    end
+  
+    def show
+    end
+  end
+end
---
+++ app/helpers/knb_auction/dashboard_helper.rb
@@ -0,0 +1,4 @@
+module KnbAuction
+  module DashboardHelper
+  end
+end
