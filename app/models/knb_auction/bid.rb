1d268377234577d95bd535130180fb0e2de3bf82
Chad Metcalf <chad@databasically.com>
2012-11-08 16:43:09 -0600

    Bid

--- app/models/knb_auction/bid.rb
+++ app/models/knb_auction/bid.rb
@@ -1,7 +1,34 @@
+# t.integer  "owner_id"
+# t.datetime "bid_at"
+# t.integer  "goodles"
+# t.integer  "auction_id"
+# t.datetime "created_at", :null => false
+# t.datetime "updated_at", :null => false
+
 module KnbAuction
   class Bid < ActiveRecord::Base
-    attr_accessible :amount, :bid_at, :owner_id
+    attr_accessible :goodles, :bid_at, :owner_id, :auction_id
+    belongs_to :owner, :class_name => "User"
+    belongs_to :auction
     
-    belongs_to :auctions
+    validate :high_bid, :on => :create
+    validate :bankroll, :on => :create
+    
+    def owner_name
+      owner.name
+    end
+    
+    def high_bid
+      if self.auction.high_bid.goodles >= goodles
+        errors.add(:goodles, "must be more than the current high bid of #{self.auction.high_bid} goodles")
+      end
+    end
+    
+    def bankroll
+      if Auction.bid_liability(owner) > owner.goodles
+        errors.add(:goodles, "must be less than your total liability of #{Auction.bid_liability(owner)}")
+      end
+    end
+    
   end
 end
