52c4936182a34f929fe6606c8b12efe8b2ae2719
Chad Metcalf <chad@databasically.com>
2012-11-14 16:06:18 -0600

    Devise for dummy app

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  def current_user
-    User.first
-  end
-  
-  def authenticate_user!
-  end
-  
-  
 end
--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
-  attr_accessible :full_name, :goodles
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :full_name, :goodles, :email, :password, :password_confirmation, :remember_me
+  # attr_accessible :title, :body
+  
+  def admin?
+    true
+  end
 end
