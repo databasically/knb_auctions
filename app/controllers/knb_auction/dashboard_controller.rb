a2be19f89fe7dd8cc8fdb568e3e0ce0b3831524e
Chad Metcalf <chad@databasically.com>
2012-11-26 12:07:55 -0600

    clean dashboard
    (this is a jumping off point for future API)

--- app/controllers/knb_auction/dashboard_controller.rb
+++ app/controllers/knb_auction/dashboard_controller.rb
@@ -3,7 +3,7 @@ require_dependency "knb_auction/application_controller"
 module KnbAuction
   class DashboardController < ApplicationController
     def index
-      redirect_to :auctionrake  => 'index' and return
+      redirect_to :auction  => 'index' and return
     end
   
     def show
